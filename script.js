c7a1fc44d7bc0c0224e143eff5ad9cf9f85b7708
“bonnielee” <bonlee8489@gmail.com>
2020-10-21 18:09:23 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let question1 = "What is a string?";
+let question2 = "What is an array?";
+let question3 = "What is an iteration?";
+let question4 = "What is a boolean?";
+let question5 = "What is a function?";
+let question6 = "What does ++ mean?";
+let question7 = "What is a conditional?";
+let question8 = "What does % represent?";
+let question9 = "What does != mean?";
+let question10 = "What does === mean?";
